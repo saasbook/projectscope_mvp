7009630cdb13156dbc4aa95ae19bc0b6ad9e97dc
tylerfreckmann <tylerfreckmann@berkeley.edu>
2016-03-10 21:44:06 +0000

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160310212951) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "git_repo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
