48cfe190f36f336777e048c177686c16c055954f
tylerfreckmann <tylerfreckmann@berkeley.edu>
2016-03-11 02:18:40 +0000

    update specs for graphics

---
+++ spec/helpers/projects_helper_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ProjectsHelper do
+  describe 'presenting the graphic of a metric' do
+    it 'should call the model method that retrieves the metric data' do
+    end
+    it 'should return some html with the class "graphic"' do
+    end
+  end
+end
