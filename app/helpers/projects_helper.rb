f7d66e3317a5e39090a3f6d0fac8dcc498fead07
tylerfreckmann <tylerfreckmann@berkeley.edu>
2016-03-16 02:39:49 +0000

    add graphic generation to ProjectsHelper#graphic

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,6 +1,20 @@
 module ProjectsHelper
 
   def graphic(metric)
-    "<div class='graphic'>#{metric.get_data}</div>"
+    data = metric.get_data
+    total_prs = data.values.sum
+    data.each do |color, value|
+      data[color] = value.to_f / total_prs
+    end
+    graph = "<div class='graphic' style = 'background: #fff;'>"
+    graph << color_block(data, :red)
+    graph << color_block(data, :yellow)
+    graph << color_block(data, :green)
+    graph << "</div>"
+    return graph
+  end
+  
+  def color_block(data, color)
+    "<div style = 'width: 25px; height: #{(data[color]*100).round}px; background: #{color}; margin: auto;'></div>"
   end
 end
