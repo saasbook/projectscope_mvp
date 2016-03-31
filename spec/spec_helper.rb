cfa65c7bacd0f4cba31975d55b2cc66efbffa04f
Daniel Xu <dhhxu@berkeley.edu>
2016-03-31 02:24:37 +0000

    Add token to travis.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 
-require 'simplecov'
 require "codeclimate-test-reporter"
-SimpleCov.start
 CodeClimate::TestReporter.start
+
+require 'simplecov'
+SimpleCov.start
 
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
