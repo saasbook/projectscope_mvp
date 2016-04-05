6a67eb94db8d0f855ff8fb252f3b52b9f9dc1d7e
tylerfreckmann <tylerfreckmann@berkeley.edu>
2016-04-05 05:52:19 +0000

    format slack graphic

--- app/assets/javascripts/slack_graphic.js
+++ app/assets/javascripts/slack_graphic.js
@@ -12,17 +12,20 @@ function drawHeatMap() {
   slack_data.addColumn('number', '');
   slack_data.addColumn('number', '');
   slack_data.addRows([
-    [11, 6, 15],
-    [0, 11, 12]
+    [11, 11, 15],
+    [10, 11, 12]
   ]);
 
   // Set chart options
   var slack_options = {'useRowLabels':false,
                  'startColor':{r:255, g:0, b:0, a:1},
-                 'endColor':{r:0, g:255, b:0, a:1}};
+                 'endColor':{r:0, g:255, b:0, a:1},
+                 'mapWidth':200,
+                 'mapHeight':200,
+                 'drawBorder': false
+  };
 
   // Instantiate and draw our chart, passing in some options.
   var chart = new org.systemsbiology.visualization.BioHeatMap(document.getElementById('heatmapContainer'));
-  // new google.visualization.PieChart(document.getElementById('heatmapContainer'));
   chart.draw(slack_data, slack_options);
 }
