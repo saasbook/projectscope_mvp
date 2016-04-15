868d94432db164a83c1abf8be6255076199aa3b1
Daniel Xu <dhhxu@berkeley.edu>
2016-04-15 06:13:20 +0000

    Add code climate metrics models as nested route in projects

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :slack_metrics
     resources :pull_requests
     resources :slack_data_points
+    resources :code_climate_metrics
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
