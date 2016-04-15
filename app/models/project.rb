7f547f16cf5b8ca1c93653d0434b8ae7c0cb4261
Daniel Xu <dhhxu@berkeley.edu>
2016-04-15 18:30:12 +0000

    Fix code climate metric typo

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   has_one :pull_request
   has_one :slack_metric
-  has_one :code_climate
+  has_one :code_climate_metric
   has_one :coverage
   has_many :slack_data_points, through: :slack_metric
 end
