a617fc41616f6368ca8eaa472bbb22d650c003b3
Daniel Xu <dhhxu@berkeley.edu>
2016-04-30 20:44:22 +0000

    Add helper method to call project CCM#score_color

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,12 @@ class Project < ActiveRecord::Base
   has_one :pivotal_tracker
   has_many :slack_data_points, through: :slack_metric
   has_one :slack_trend
+  
+  def code_climate_color
+      if self.code_climate_metric
+          self.code_climate_metric.score_color
+      else
+          ""
+      end
+  end
 end
